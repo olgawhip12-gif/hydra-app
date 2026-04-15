bf243e0a501bdf96e554c91b759a8cd1c5759ad3
olgawhip12-gif <olgawhip12@gmail.com>
2026-04-15 17:00:55 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,2 +1,41 @@
-self.addEventListener('install', e => self.skipWaiting());
-self.addEventListener('fetch', e => {});
+const CACHE_NAME = 'hydra-cache-v1';
+const urlsToCache = [
+  'index.html',
+  'manifest.json'
+];
+
+// Встановлення
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+  self.skipWaiting();
+});
+
+// Активація
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+  return self.clients.claim();
+});
+
+// Обробка запитів
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
